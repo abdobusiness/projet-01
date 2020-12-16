3ccaf297954daf37fa2355b04901e85050fa3f02
abdo business <abdobusiness9@gmail.com>
2020-12-16 02:29:16 +0000

    last commit

--- javacript.js
+++ javacript.js
@@ -6,3 +6,9 @@
 var parent = document.getElementById('a') ;
 var btn    =document.getElementById('btn')  ;
 
+ btn.onclick = function () {
+ 
+
+  parent.children[0].nextElementSibling.style.backgroundColor ='orange'
+ }
+
